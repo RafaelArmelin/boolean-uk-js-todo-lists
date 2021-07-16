9d6d1d836bbad7dbc2446213cba25a1f71869e38
Rafael Armelin <raffael.armelin@gmail.com>
2021-07-16 13:12:03 +0100

    finished

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ console.log("users: ", users);
 console.log("todos: ", todos);
 
 
-// - Display an alert with the username and all the todos titles that belong to that user 
 
 // - Create an alert that lists all users, with their ids, names and what city they're from
 
@@ -28,15 +27,49 @@ const row = `${id} | ${name} | ${city} \n`;
 
 message = message + row;
 
-console.log(message)
+// console.log(message)
 }
 
  alert(message);
 
- console.log(users[0].id , users[0].name , users[0].address.city);
+//  console.log(users[0].id , users[0].name , users[0].address.city);
 
 
 
  //- Prompt the user for a user id
 
  let userId = prompt("Who's ID do you wanna check? Use and ID number.");
+
+
+ // - Display an alert with the username and all the todos titles that belong to that user 
+
+ let messageAlert = "";
+
+ for (let i=0; i<todos.length; i++){
+    const todosUser = todos[i].userId;
+    const todosId = todos[i].id;
+    const todosTitle = todos[i].title;
+    const todosCompleted = todos[i].completed;  
+
+    const row = `\n ${todosUser} | ${todosId} | ${todosTitle} | ${todosCompleted}`;
+    
+    // console.log(row);
+
+    if(todosUser === Number(userId)){
+        messageAlert = messageAlert+row;
+    }
+}
+    let messageName ="";
+
+for (let i=0; i<users.length; i++){
+    const surname = users[i].username;
+    const idSurname = users[i].id;
+
+    if(idSurname === Number(userId)){
+        messageName = messageName + surname ;
+        console.log(messageName);
+    }
+
+}
+
+ alert(messageName+messageAlert);
